9f6a9fbcd73e6adb8886ae533511ccd33bdab132
A <A@DESKTOP-56JRQFU>
2022-09-21 20:28:27 +0900

    초기 커밋.

---
+++ Project1/main.cpp
@@ -0,0 +1,32 @@
+﻿#include "gl/glut.h"
+#include <math.h>
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutCreateWindow
+	("OpenGL");
+	glutDisplayFunc(display_circle);
+	glutMainLoop();
+	return 0;
+}
+
+void display_circle() { 
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0f, 0.0f, 1.0f);
+
+	double rad = 1;
+	glBegin(GL_LINE_STRIP);
+
+	for (int i = 0; i < 360; i++) { 
+		double angle, x, y;
+		angle = i * 3.141592 / 180;
+		x = rad * cos(angle);
+		y = rad * sin(angle);
+		glVertex2f(x, y); 
+	}
+	glEnd();
+	glFinish(); 
+}
+
